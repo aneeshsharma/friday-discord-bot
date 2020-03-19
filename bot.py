234709fc69090c1e568af3c4bfee8eae2a3814de
Anish Sharma <aneeshsharma15@outlook.com>
2020-03-19 21:59:05 +0530

    Made the path to conig.json correctly point relative to the script

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 import json
 import SocketInterface
+import os
 
 client = discord.Client()
 
@@ -12,20 +13,25 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	if message.author == client.user:
-		return
-	if message.content.startswith('Hey Friday,'):
-		msg = message.content[10:].strip()
-		result = "Cannot Think Right now"
-		try:
-			sock.send(msg)
-			result = sock.recv()[0]
-		except:
-			print('Unable to send')
+    if message.author == client.user:
+        return
+    if message.content.startswith('Hey Friday,'):
+        msg = message.content[11:].strip()
+        result = "Cannot Think Right now"
+        try:
+            sock.send(msg)
+            result = sock.recv()[0]
+        except:
+            print('Unable to send')
 
-		await message.channel.send(result.decode('utf-8'))
+        await message.channel.send(result.decode('utf-8'))
 
-config_file = open("config.json", "r")
+dir = os.path.dirname(__file__)
+config_filename = "config.json"
+
+abs_path = os.path.join(dir, config_filename)
+
+config_file = open(abs_path, "r")
 config = json.loads(config_file.read())
 
 sock = SocketInterface.Socket()
